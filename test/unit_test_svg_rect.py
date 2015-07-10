05f9ba1fadd7a219d8148a0d254ba15b91f8eeab
CWBudde <Christian@savioursofsoul.de>
2015-07-10 23:05:13 +0200

    Fixed issue with Inkscape 0.91 (see http://wiki.inkscape.org/wiki/index.php/Release_notes/0.91#Units:_Breaking_change)

--- test/unit_test_svg_rect.py
+++ test/unit_test_svg_rect.py
@@ -1,4 +1,3 @@
-
 import unittest
 import sys
 
@@ -12,7 +11,7 @@ class TestRect(unittest.TestCase):
     def setUp(self):
         self.effect = Effect()
         self.document = None
-        self.effect.parse("TestFiles/unit_test_svg_Rect_WithRxRy.svg")
+        self.effect.parse("TestFiles/unit_test_svg_Rect_ComRxRy.svg")
         self.node = None
         self.canvas = Canvas(0, 0)
               
@@ -28,7 +27,7 @@ class TestRect(unittest.TestCase):
                 return node
         
     def testExitWithoutRxRy(self):
-        self.document = self.effect.parse("TestFiles/unit_test_svg_Rect_WithoutRxRy.svg")
+        self.document = self.effect.parse("TestFiles/unit_test_svg_Rect_SemRxRy.svg")
         root = self.effect.document.getroot()
         self.rect = Rect(None, self.node, self.canvas, None)
         self.rect.node = self.findNodeInG(root, 'rect')
@@ -41,7 +40,7 @@ class TestRect(unittest.TestCase):
         self.assertEqual(ry, 0)
         
     def testExitWithRxRy(self):
-        self.document = self.effect.parse("TestFiles/unit_test_svg_Rect_WithRxRy.svg")
+        self.document = self.effect.parse("TestFiles/unit_test_svg_Rect_ComRxRy.svg")
         root = self.effect.document.getroot()
         self.rect = Rect(None, self.node, self.canvas, None)
         self.rect.node = self.findNodeInG(root, 'rect')
@@ -52,7 +51,7 @@ class TestRect(unittest.TestCase):
         self.assertEqual(h, 150.0)
         self.assertEqual(rx, 5.0)
         self.assertEqual(ry, 10.0)
-    
+
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
